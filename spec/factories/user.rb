9d8bd3f2015d95df3b6822d01135b03523c9d3f3
André Carvalho <andregac993@gmail.com>
2025-04-23 20:37:49 -0300

    test: add test model user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { "Usuário Teste" }
     sequence(:email) { |n| "user#{n}@example.com" }
-    password { "Password123" }
+    password { "Passw" }
     password_confirmation { "Password123" }
   end
 end
